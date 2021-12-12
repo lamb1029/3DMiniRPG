2a2be8bc16f4d526216e03264b8bd510a443e26b
lamb1029 <90374849+lamb1029@users.noreply.github.com>
2021-12-12 22:53:35 +0900

    PoolManager

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -9,11 +9,13 @@ public class Managers : MonoBehaviour
     static Managers Instance { get { Init(); return s_instance; } }
 
     InputManager _input = new InputManager();
+    PoolManager _pool = new PoolManager();
     ResourceManager _resource = new ResourceManager();
     SceneManagerEX _scene = new SceneManagerEX();
     SoundManager _sound = new SoundManager();
     UIManager _ui = new UIManager();
     public static InputManager Input { get { return Instance._input; } }
+    public static PoolManager Pool { get { return Instance._pool; } }
     public static ResourceManager Resource { get { return Instance._resource; } }
     public static SceneManagerEX Scene { get { return Instance._scene; } }
     public static SoundManager Sound { get { return Instance._sound; } }
@@ -42,6 +44,7 @@ public class Managers : MonoBehaviour
             DontDestroyOnLoad(go);
             s_instance = go.GetComponent<Managers>();
 
+            s_instance._pool.Init();
             s_instance._sound.Init();
         }
     }
@@ -52,5 +55,6 @@ public class Managers : MonoBehaviour
         Scene.Clear();
         Sound.Clear();
         UI.Clear();
+        Pool.Clear();
     }
 }
--- Assets/Scripts/Managers/ResourceManager.cs
+++ Assets/Scripts/Managers/ResourceManager.cs
@@ -6,22 +6,40 @@ public class ResourceManager
 {
     public T Load<T>(string path) where T : Object
     {
+        if(typeof(T) == typeof(GameObject))
+        {
+            string name = path;
+            int index = name.LastIndexOf("/");
+            if (index >= 0)
+                name = name.Substring(index + 1);
+
+            GameObject go = Managers.Pool.GetOriginal(name);
+            if (go != null)
+                return go as T;
+        }
+
         return Resources.Load<T>(path);
     }
 
     public GameObject Instantiate(string path, Transform parent = null)
     {
-        GameObject prefab = Load<GameObject>($"Prefabs/{path}");
-        if(prefab == null)
+        //original을 미이 들고 있다면 바로 사용
+        GameObject orgonal = Load<GameObject>($"Prefabs/{path}");
+        if(orgonal == null)
         {
             Debug.Log($"Failed to load prefab : {path}");
             return null;
         }
 
-        GameObject go = Object.Instantiate(prefab, parent);
-        int index = go.name.IndexOf("(Clone)");
-        if (index > 0)
-            go.name = go.name.Substring(0, index);
+        //혹시 폴링된 애가 있을까?
+        if (orgonal.GetComponent<Poolable>() != null)
+            return Managers.Pool.Pop(orgonal, parent).gameObject;
+
+        GameObject go = Object.Instantiate(orgonal, parent);
+        //int index = go.name.IndexOf("(Clone)");
+        //if (index > 0)
+        //    go.name = go.name.Substring(0, index);
+        go.name = orgonal.name;
 
         return go;
     }
@@ -30,6 +48,14 @@ public class ResourceManager
     {
         if (obj == null)
             return;
+
+        Poolable poolable = obj.GetComponent<Poolable>();
+        if(poolable != null)
+        {
+            Managers.Pool.Push(poolable);
+            return;
+        }
+
         Object.Destroy(obj);
     }
 }
